d72d9677994ec77bdb8ea7b5e9e3663c01b14eb1
brunoandradedev <brunof.andrade1@hotmail.com>
2025-03-20 20:49:58 -0300

    Adicionando código para função lambda

--- index.js
+++
@@ -1,6 +0,0 @@
-import { handler } from "./src/functions/authenticate.js";
-
-console.log('Running test');
-const result = await handler({ userName: '35843258890', password: '!Teste1234' })
-console.log('Result:', result);
-console.log('Test complete');
