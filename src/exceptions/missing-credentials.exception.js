19be6ddb1b375de79c91ed3729973401970b94e1
Bruno Flávio <brunof.andrade1@hotmail.com>
2025-03-22 11:26:23 -0300

    Adicionando função lambda para autenticação com Cognito (#1)

---
+++ src/exceptions/missing-credentials.exception.js
@@ -0,0 +1,6 @@
+export class MissingCredentialsException extends Error {
+    constructor(message = 'Username and password are required') {
+        super(message);
+        this.name = 'MissingCredentialsException';
+    }
+}
